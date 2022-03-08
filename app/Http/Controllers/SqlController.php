5f53e27a99753f168b88b9ae7fc09a35325e324d
dmtechjcornejo <jcornejo@dmtech.cl>
2022-03-08 15:37:47 -0300

    Primer Commit

---
+++ app/Http/Controllers/SqlController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class SqlController extends Controller
+{
+    public function index(){
+        $users = DB::select('select * from users');
+        return view('welcome', ['users' => $users]);
+        
+    }
+}
